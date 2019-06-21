8d768eebe3a3f6eab93d9a0c8bcde98a8fde99cf
xinxiamu <932852117@qq.com>
2019-06-21 16:22:08 +0800

    重构

--- src/main/java/com/xrlj/apigateway/config/WebConfig.java
+++ src/main/java/com/xrlj/apigateway/config/WebConfig.java
@@ -1,9 +1,6 @@
 package com.xrlj.apigateway.config;
 
-import com.xrlj.framework.config.AbstractWebConfiguration;
-import com.xrlj.framework.config.JsonHandlerExceptionResolverOpen;
-import com.xrlj.framework.config.JsonHttpMessageConverter2;
-import com.xrlj.framework.config.JsonViewHttpMessageConverter;
+import com.xrlj.framework.config.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -17,17 +14,7 @@ import java.util.List;
  * 这种方式会屏蔽springboot的@EnableAutoConfiguration中的设置
  */
 @Configuration
-public class WebConfig extends AbstractWebConfiguration {
-
-    @Autowired
-    @Qualifier("jsonViewHttpMessageConverterOpen")
-    private JsonViewHttpMessageConverter jsonViewHttpMessageConverterOpen;
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-       converters.add(jsonViewHttpMessageConverterOpen);
-        converters.add(new JsonHttpMessageConverter2());
-    }
+public class WebConfig extends AbstractOutWebConfiguration {
 
     /**
      * 统一异常处理。
