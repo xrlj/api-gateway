1d2dd4913047ac76f33022a6be8439c11bdd771a
xinxiamu <932852117@qq.com>
2019-06-26 17:19:22 +0800

    重构

---
+++ src/main/java/com/xrlj/apigateway/config/MyNotFoundException.java
@@ -0,0 +1,8 @@
+package com.xrlj.apigateway.config;
+
+import com.xrlj.framework.config.NotFoundException;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyNotFoundException extends NotFoundException {
+}
