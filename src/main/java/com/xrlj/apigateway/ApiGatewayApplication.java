abd09b089690ba30a795776fed3e9ebbcec8488e
xinxiamu <932852117@qq.com>
2019-07-08 15:49:14 +0800

    重构

--- src/main/java/com/xrlj/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/xrlj/apigateway/ApiGatewayApplication.java
@@ -1,6 +1,7 @@
 package com.xrlj.apigateway;
 
 import com.xrlj.framework.base.BaseSpringbootApplication;
+import com.xrlj.framework.config.FeignConfiguration;
 import com.xrlj.framework.spring.mvc.api.ApiResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +30,7 @@ import java.util.Locale;
  */
 @Slf4j
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.xrlj.apigateway", "com.xrlj.framework"})
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {FeignConfiguration.class}),basePackages = {"com.xrlj.apigateway", "com.xrlj.framework"})
 @EnableDiscoveryClient
 @EnableEurekaClient //可注册到服务中心
 @EnableZuulProxy
